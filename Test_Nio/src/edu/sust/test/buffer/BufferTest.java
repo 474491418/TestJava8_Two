83362efa805bdaada7023d8cc76afb14b41901e2
474491418 <474491418@qq.com>
2015-07-17 17:02:08 +0800

    to change

--- Test_Nio/src/edu/sust/test/buffer/BufferTest.java
+++ Test_Nio/src/edu/sust/test/buffer/BufferTest.java
@@ -7,14 +7,24 @@ import java.nio.CharBuffer;
  * used by TestJava8_Two
  */
 public class BufferTest {
-    char[] myArray = new char[100];
+    char[] myArray = new char[10];
     CharBuffer charBuffer = CharBuffer.wrap(myArray);
 
     public void test() {
         charBuffer.put('s');
-        myArray[1]='a';
+        myArray[1] = 'a';
+        myArray[2] = 'e';
+        myArray[3] = 'q';
+        myArray[4] = 'n';
         System.out.println(myArray[0]);
         System.out.println(charBuffer.get(1));
+        System.out.println(charBuffer.arrayOffset());
+        charBuffer.position(2).limit(6);
+        charBuffer.slice();
+        System.out.println(charBuffer.length());
+
+        for (int i = 0; i < charBuffer.length(); i++) System.out.println(charBuffer.get(i) + " " + i);
+
     }
 
     public static void main(String[] args) {
